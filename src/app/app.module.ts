36047e3691760e7763fbfc58042888ba10807d25
moore1474 <moore.1474@gmail.com>
2019-07-28 16:12:14 -0700

    Removed hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { CardComponentComponent } from './card-component/card-component.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CardComponentComponent ],
+  declarations: [ AppComponent, CardComponentComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
