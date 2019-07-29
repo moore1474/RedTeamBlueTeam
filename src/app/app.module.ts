19410b6fcbba7a1d6685bf48ec61b749846194a6
moore1474 <moore.1474@gmail.com>
2019-07-28 17:54:27 -0700

    Added play area.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponentComponent } from './card-component/card-component.component';
+import { PlayAreaComponent } from './play-area/play-area.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, CardComponentComponent ],
+  declarations: [ AppComponent, CardComponentComponent, PlayAreaComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
