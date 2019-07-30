c8dd20f601e2e4c7ce8e6d0adc96cd29b73b5384
moore1474 <moore.1474@gmail.com>
2019-07-29 20:43:22 -0700

    Added Turn.

--- src/app/model/game.ts
+++ src/app/model/game.ts
@@ -1,10 +1,12 @@
 import { Player } from './player';
 import { Team } from './card';
 import { Cards, CARDS} from './cards';
+import {Turn} from './turn';
 
 export class Game {
 
   readonly player1 = new Player('Computer', Team.BLUE, CARDS.blue);
   readonly player2 = new Player('Nick', Team.RED, CARDS.red);
+  readonly turn = new Turn();
 
 }
---
+++ src/app/model/turn.ts
@@ -0,0 +1,17 @@
+import { Card } from './card';
+
+export enum Phase {
+  SELECT_PHASE_1 = 1,
+  RESOLVE_PHASE_2 = 2,
+  DISCARD_PHASE_3
+}
+
+export class Turn{
+
+  phase = Phase.SELECT_PHASE_1;
+  player_1_selected: Card;
+  player_2_selected: Card;
+
+
+
+}
