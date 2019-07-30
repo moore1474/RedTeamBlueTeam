3b0475da83234162af49e71673d463655d4ed6bd
moore1474 <moore.1474@gmail.com>
2019-07-29 20:55:15 -0700

    Added status message.

--- src/app/model/turn.ts
+++ src/app/model/turn.ts
@@ -6,12 +6,21 @@ export enum Phase {
   DISCARD_PHASE_3
 }
 
-export class Turn{
+export class Turn {
 
   phase = Phase.SELECT_PHASE_1;
   player_1_selected: Card;
   player_2_selected: Card;
 
+  constructor(){
+  
+  }
 
+  public getPlayer1Status(){
+    if (this.phase == Phase.SELECT_PHASE_1) {
+      return "select your card";
+    }
+    return "";
+  }
 
 }
