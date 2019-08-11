37b277819901268d778894ec8dbfb269a9d55124
moore1474 <moore.1474@gmail.com>
2019-08-10 21:23:11 -0700

    Changed card to use angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CardComponentComponent } from './card-component/card-component.component';
 import { PlayAreaComponent } from './play-area/play-area.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule],
   declarations: [ AppComponent, CardComponentComponent, PlayAreaComponent ],
   bootstrap:    [ AppComponent ]
 })
