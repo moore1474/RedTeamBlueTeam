1d5053558f454c978c93bb17e7f952f6607700ab
moore1474 <moore.1474@gmail.com>
2019-10-02 22:24:38 -0700

    Fixed second player card selection.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,16 @@ export class AppComponent {
 
   cardSelected(card) {
     this.game.turn.player_1_selected = card;
+    console.log("Player 1 selected card");
     setTimeout(() => {
+      console.log("Player 1 card removed");
       this.game.player1.hand.removeCard(card);
     }, 400);
-  
+    setTimeout(() => {
+      this.game.turn.player_2_selected = this.game.player2.hand.randomCard();
+      console.log("Player 2 Card selected")
+      this.game.player2.hand.removeCard(this.game.turn.player_2_selected);
+    }, 800);
 
   }
 
